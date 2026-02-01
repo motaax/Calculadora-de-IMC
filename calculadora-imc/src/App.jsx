06130a1113b9e7468f874544f470b7aa772b205f
Felipe Mota <felipemota465@gmail.com>
2026-02-01 15:48:37 -0300

    Commitando

--- calculadora-imc/src/App.jsx
+++ calculadora-imc/src/App.jsx
@@ -16,6 +16,23 @@ function App() {
     const imcResult = (weightFloat / (heightFloat * heightFloat)).toFixed(1)
 
     setImc(imcResult)
+
+    data.forEach((item) => {
+      if(imcResult >= item.min && imcResult <= item.max) {
+        setInfo(item.info)
+        setInfoClass(item.infoClass)
+      }
+    })
+
+    if(!info) return
+  }
+
+  const resetCalc = (e) => {
+    e.preventDefault()
+
+    setImc("")
+    setInfo("")
+    setInfoClass("")
   }
 
   const [imc, setImc] = useState("")
@@ -24,7 +41,7 @@ function App() {
 
   return (
     <div className='container'>
-      {!imc ?(<ImcCalc calcImc={calcImc}/>): (<ImcTable data={data}/>)}
+      {!imc ?(<ImcCalc calcImc={calcImc}/>): (<ImcTable data={data} imc={imc} info={info} infoClass={infoClass} resetCalc={resetCalc}/>)}
     </div>
   )
 }
